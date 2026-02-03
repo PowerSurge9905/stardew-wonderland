cc212c577847e2216b5e5f73d1fb65f403a44818
PowerSurge9905 <powersurge9905@gmail.com>
2026-02-02 22:19:36 -0500

    Version 0.0.0
    Project Start

---
+++ StardewWonderland/StardewWonderland/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewWonderland
+{
+    internal sealed class ModEntry : Mod
+    {
+        // Mod entry point, called when the mod is first loaded
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
